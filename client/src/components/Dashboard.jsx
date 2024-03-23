dbad19c56e3a0780e31fa2887669752744c69e3e
Konstantin Nochevny <kosnobiz@gmail.com>
2024-03-23 16:34:26 +0200

    Render expenses from the server

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,28 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { loadExpenses } from '../store/actions/expense.action.js'
+
 function Dashboard() {
+    const dispatch = useDispatch()
+    const { expenses } = useSelector(state => state.expenseModule)
+
+    useEffect(() => {
+        dispatch(loadExpenses())
+    }, [dispatch])
+
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    }
+
     return (
         <div>
             <h1>This is the dashboard 🧮🪙</h1>
+            <ul>
+                {expenses.map(expense => (
+                    <li key={expense._id}>{expense.name}: {expense.amount} - {formatDate(expense.date)}</li>
+                ))}
+            </ul>
         </div>
     )
 }
