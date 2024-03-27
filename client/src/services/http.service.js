fd4ca7bd937cdd17481319e97659248867b3faaa
Konstantin Nochevny <kosnobiz@gmail.com>
2024-03-28 00:22:53 +0200

    Enhance UX by redirecting on expired session

--- client/src/services/http.service.js
+++ client/src/services/http.service.js
@@ -10,17 +10,27 @@ const axiosInstance = Axios.create({
     baseURL: BASE_URL
 })
 
-axiosInstance.interceptors.request.use(function (config) {
-    // Retrieving the token from localStorage
-    const token = localStorage.getItem('token')
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    }, 
+    (error) => Promise.reject(error)
+)
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('token')
+            window.location = '/'
+        }
+        return Promise.reject(error)
     }
-    return config
-}, function (error) {
-    // Do something with request error
-    return Promise.reject(error)
-})
+)
 
 export const httpService = {
     get(endpoint, data) {
