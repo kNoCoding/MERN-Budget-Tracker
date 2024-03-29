5cdb992849d0b5317ca774d885bd2d3ad735a632
Konstantin Nochevny <kosnobiz@gmail.com>
2024-03-29 13:32:08 +0300

    Implement toggle between login and registration forms

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -4,6 +4,7 @@ import { httpService } from '../services/http.service'
 
 
 function AuthForm() {
+  const [isLogin, setIsLogin] = useState(true)
   const [credentials, setCredentials] = useState({ email: '', password: '' })
   const navigate = useNavigate()
 
@@ -17,37 +18,45 @@ function AuthForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+    const authEndpoint = isLogin ? '/auth/login' : '/auth/register'
     try {
-      const response = await httpService.post('/auth/login', credentials)
-      console.log('Login/Register successful:', response)
+      const response = await httpService.post(authEndpoint, credentials)
+      console.log('Auth successful:', response)
       const { token } = response
       localStorage.setItem('token', token)
       console.log('FUCK YEAHHHHHH~!!!!')
       navigate('/dashboard')
     } catch (error) {
-      console.error('Login/Register failed:', error.response ? error.response.data : error)
+      console.error('Auth failed:', error.response ? error.response.data : error)
       // Handle error (e.g., display error message)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="email"
-        placeholder="Username"
-        value={credentials.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={credentials.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login/Register</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email/Username"
+          value={credentials.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+
+        <p onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? 'New user? Register' : 'Already registered? Login'}
+        </p>
+        <br />
+        <button type="submit">{isLogin ? 'Login' : 'Register'}</button>
+      </form>
+    </>
   )
 }
 
