33bdc62b3e66861dfeef6e07039b656768375b5f
Konstantin Nochevny <kosnobiz@gmail.com>
2024-11-08 20:11:08 +0200

    Changes to the budgetSchema

--- server/models/budget.model.js
+++ server/models/budget.model.js
@@ -3,28 +3,38 @@ import mongoose from 'mongoose'
 const { ObjectId } = mongoose.Schema.Types
 
 const budgetSchema = new mongoose.Schema({
-    totalAmount: {
-        type: Number,
-        required: true,
-    },
-    categories: [{
-        name: String,
-        allocatedAmount: Number,
-    }],
-    startDate: {
-        type: Date,
-        required: true,
-    },
-    endDate: {
-        type: Date,
-        required: true,
-    },
+    category: String,
+    amount: Number,
     user: {
         type: ObjectId,
         ref: 'User',
         required: true,
     }
 })
+
+// const budgetSchema = new mongoose.Schema({
+//     totalAmount: {
+//         type: Number,
+//         required: true,
+//     },
+//     categories: [{
+//         name: String,
+//         allocatedAmount: Number,
+//     }],
+//     startDate: {
+//         type: Date,
+//         required: true,
+//     },
+//     endDate: {
+//         type: Date,
+//         required: true,
+//     },
+//     user: {
+//         type: ObjectId,
+//         ref: 'User',
+//         required: true,
+//     }
+// })
 
 const Budget = mongoose.model('Budget', budgetSchema)
 
